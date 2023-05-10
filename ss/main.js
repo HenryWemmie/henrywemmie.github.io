88b4a1cc7b86b9dd48ca9d31d6e5f08ac81c0e70
Hankypoo7 <100436822+Hankypoo7@users.noreply.github.com>
2023-05-10 07:59:17 -0500

    Update main.js

--- ss/main.js
+++ ss/main.js
@@ -2,7 +2,7 @@ const options = {
   decimalPlaces: 5,
   duration: 3600,
 };
-let demo = new CountUp('myTargetElement', 0.73, options);
+let demo = new CountUp('target', 0.73, options);
 if (!demo.error) {
   demo.start();
 } else {
