81f9b4820d983c998ef577efcac6b75087e25c6e
Hankypoo7 <100436822+Hankypoo7@users.noreply.github.com>
2023-05-10 08:41:58 -0500

    Update main.js

--- ss/main.js
+++ ss/main.js
@@ -1,4 +1,4 @@
-const options = {useEasing: false,   decimalPlaces: 5,   duration: 3600, };
+const options = {  decimalPlaces: 5,   duration: 3600, };
 
 window.onload = function() {
   var numAnim = new countUp.CountUp('target', 0.73, options);
