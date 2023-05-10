8f6c1cbb6a67480662b9c15ca929f9889ea91ed7
Hankypoo7 <100436822+Hankypoo7@users.noreply.github.com>
2023-05-10 09:06:07 -0500

    Update main.js

--- ss/main.js
+++ ss/main.js
@@ -1,7 +1,8 @@
 window.onload = function() {
-var numAnim = new countUp.CountUp('target', 0.73, { decimalPlaces: 5, duration: 3600 });
+  var numAnim = new countUp.CountUp('target', 2000);
   if (!numAnim.error) {
     numAnim.start();
   } else {
     console.error(numAnim.error);
   }
+}
