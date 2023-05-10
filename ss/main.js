eee0c2fd5f0861de8a1e090a6315205f64602aff
Hankypoo7 <100436822+Hankypoo7@users.noreply.github.com>
2023-05-10 09:11:55 -0500

    Update main.js

--- ss/main.js
+++ ss/main.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-  var numAnim = new countUp.CountUp('target', 2000);
+  var numAnim = new countUp.CountUp('target',useEasing: false ,2000);
   if (!numAnim.error) {
     numAnim.start();
   } else {
